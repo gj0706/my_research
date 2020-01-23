f52b0e82ecedce45f4dcf8ed809d1f86788f6743
Jian Guo <guojian0706@gmail.com>
2020-01-23 10:56:58 -0600

    added all the stream graphs for each feature

--- neural_network/js/stream.js
+++ neural_network/js/stream.js
@@ -1,6 +1,7 @@
-const sMargin = {top: 15 , right: 5, bottom: 20, left: 20};
-const sHeight = 80 - sMargin.top - sMargin.bottom;
-const sWidth = 80 - sMargin.left - sMargin.right;
+// const sMargin = {top: 20 , right: 5, bottom: 20, left: 20};
+const sMargin = {top: 0 , right: 0, bottom: 0, left: 0};
+const sHeight = 35 - sMargin.top - sMargin.bottom;
+const sWidth = 35 - sMargin.left - sMargin.right;
 
 
 d3.json('data/all_epochs.json').then(function(data){
@@ -8,19 +9,58 @@ d3.json('data/all_epochs.json').then(function(data){
     // const feat1i= data.filter(d=>d.feature === 0);
     // console.log(nested);
     // debugger
-    draw_stream_chart(data,0,'i');
+    let cell = data.filter(d=>d.feature === 0 && d.cell === 1);
+    // draw_stream_chart(data,0);
+
+    // draw_single_chart(data, 0, 0);
+    for(let j = 0; j < 8; j++){
+        for(let i = 0; i < 50; i++){
+            draw_single_chart(data, `#stream${j+1}`, j, i);
+        }
+    };
 });
 
-function draw_stream_chart(data, feature_num, gate){
-    let nestedData = d3.nest().key(k=>k.cell).entries(data.filter(v=>v.feature === feature_num && v.gate === gate));
-    // const allKeys = nestedData.map(d=>d.key);
-    // const filter = nested.map(d=>d.values.filter(v=>v.feature === 0 && v.gate === 'i'));
+function draw_single_chart(data, selector, nFeat, nCell){
+    // Data should be per cell per feature
+    let filtered = data.filter(d=>d.feature === nFeat && d.cell === nCell);
+    let nested = d3.nest().key(k=>k.gate).entries(filtered);
+    let n = d3.group(filtered, d=>d.gate);
+
+    const keys = nested.map(d=>d.key); // ["i", "f", "c", "o"] One stream each key
+    const values = Array.from(d3.rollup(filtered, ([d]) => Math.abs(d.value), d => d.epoch, d => d.gate));
+
+    // Define stack layout
+    let series = d3.stack()
+        .keys(keys)
+        .value(([, values], key) => values.get(key))
+        // .offset(d3.stackOffsetDiverging)
+        .offset(d3.stackOffsetSilhouette)
+        (values);
+
     debugger
+    // Define X scale --> epochs
+    let x = d3.scaleLinear()
+        .domain(d3.extent(filtered, d=>d.epoch))
+        .range([ 0, sWidth ]);
+
+    // let y = d3.scaleOrdinal()
+    //     // .domain(d3.extent(series.flat().flat()))
+    //     .domain(["i","f", "c", "o"])
+    //     .range([ sHeight,sHeight/4,sHeight/2, sHeight/4*3]);
+    let y = d3.scaleLinear()
+        .domain([-1, 1])
+        // .domain([0, d3.max(series, d=>d3.max(d, d=>(d[1])))]).nice()
+        .range([ sHeight ,0 ]);
+    //
+    let color = d3.scaleOrdinal()
+        .domain(keys)
+        .range(d3.schemeCategory10);
+
     // Defile area
     let area = d3.area()
-        .x(d=>d.epoch)
-        .y0(sHeight)
-        .y1(d=>d.value)
+        .x(d=>x(d.data[0]))
+        .y0(d=>y(d[0]))
+        .y1(d=>y(d[1]))
         .curve(d3.curveCardinal);
 
     // Define line
@@ -28,108 +68,41 @@ function draw_stream_chart(data, feature_num, gate){
         .x(d=>d.epoch)
         .y(d=>d.value);
 
-    // Add an svg element for each group. They will be one beside each other and will go on the next row when no more room available
-    let sSvg = d3.select("#streamChart")
-        .selectAll(".uniqueChart")
-        .data(nestedData)
-        .enter()
+
+    let svg = d3.select(selector)
         .append("svg")
         .attr("width", sWidth + sMargin.left + sMargin.right)
         .attr("height", sHeight + sMargin.top + sMargin.bottom)
         .append("g")
         .attr("transform", "translate(" + sMargin.left + "," + sMargin.top + ")");
 
+    // let tip = d3.tip()
+    //     .attr('class', 'd3-tip')
+    //     .offset([-10, 0])
+    //     .html(function(d) {
+    //         return
+    //         "Gate: <span>" + d.key + "</span>"+ "<br>"});
 
-    // Add X axis --> epochs
-    let x = d3.scaleLinear()
-        .domain(d3.extent(data, d=>d.epoch))
-        .range([ 0, sWidth ]);
-
-    //Add Y axis --> weight values
-    let y = d3.scaleLinear()
-        .domain([-1, 1])
-        .range([ sHeight, 0 ]);
-
-    // Draw x and y axis on g elements
-    sSvg.append("g")
+    // Draw x and y axis
+    svg.append("g")
         .attr("transform", "translate(0," + sHeight + ")")
         .call(d3.axisBottom(x).ticks(3));
-    sSvg.append("g")
-        .call(d3.axisLeft(y).ticks(5));
+    svg.append("g")
+        .call(d3.axisLeft(y).ticks(3));
 
-    let color = d3.scaleSequential(d3.interpolateRdBu)
-        .domain([-1,1]);
-
-    sSvg.append("path")
-        .attr("class","area")
-        // .style("fill", function(d,i){ return color(d.values[i].value)})
-        // .style("fill", function(d,i){ return (d.values[i].value >= 0 ? "blue":"red")})
-        // .style("stroke", "none")
-        .attr("d", function(d){
-            return d3.area()
-                .curve(d3.curveBasis)
-                .x(d=>x(d.epoch))
-                .y0(sHeight)
-                .y1(d=>y(d.value))
-                (d.values)});
-
-    sSvg.append("path")
-        .attr("class", "line")
-        // .attr("stroke", "red")
-        // .attr("fill", "none")
-        .attr("d", function(d){
-                return d3.line()
-                    .x(d=>x(d.epoch))
-                    .y(d=>y(d.value))
-                    (d.values)});
-
-
-    // Add titles
-    sSvg
-        .append("text")
-        .attr("text-anchor", "start")
-        .attr("y", -5)
-        .attr("x", 0)
-        .text(function(d){ return(`cell ${d.key}`)})
-        .style("fill", function(d){ return color(d.key) })
-
-
-
-
-    // // pick one of the data series
-    // const dataSeries = nested[7].values.filter(d=>d.feature === 0 && d.gate === 'i');
-    // debugger
-    // // use the d3fc extent component to compute the x & y domain
-    // const yExtent = fc.extentLinear()
-    //     .accessors([d => d.value])
-    //     .pad([0, 0.2])
-    //     .include([0]);
-    //
-    // const xExtent = fc.extentLinear()
-    //     .accessors([d => d.epoch]);
-    //
-    // const area =  fc.seriesSvgArea()
-    //     .crossValue(d => d.epoch)
-    //     .mainValue(d => d.value);
-    //
-    // const line = fc.seriesSvgLine()
-    //     .crossValue(d => d.epoch)
-    //     .mainValue(d => d.value);
-    //
-    // const chart = fc.chartSvgCartesian(
-    //     d3.scaleLinear(),
-    //     d3.scaleLinear())
-    //     .yDomain(yExtent(data))
-    //     .xDomain(xExtent(data))
-    //     .yOrient('left')
-    //     .xOrient('bottom')
-    //     .plotArea(line);
-    //
-    // // render
-    // d3.select('#streamChart')
-    //     .datum(dataSeries)
-    //     .call(chart);
-    //
-    //
+    // draw stream graph
+    svg.append("g").selectAll("path")
+        .data(series)
+        .join("path")
+        .attr("class","streams")
+        // .attr("transform", (d,i)=>"translate(0," + y(i) + ")")
+        .attr("fill", ({key})=>color(key))
+        .attr("d", area)
+        // .on("mouseover", tip.show)
+        // .on("mouseout", tip.hide);
+        .append("title")
+        .text(({key})=>key);
 
 }
+
+
